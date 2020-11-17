82cdf15b57d70273049f5fdb75f7c8f089d73590
apurva-virgincar <57170815+apurva-virgincar@users.noreply.github.com>
2020-11-17 20:58:46 +0530

    Add files via upload

---
+++ build/precache-manifest.41960421a86dc0d8dc7843ccd26f2e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4409114aee5a2513afe14c648c4d940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388e937847dde68830b8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0328b553444c1740de30",
+    "url": "/static/js/2.85f54f8a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.85f54f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388e937847dde68830b8",
+    "url": "/static/js/main.16f33a8b.chunk.js"
+  },
+  {
+    "revision": "559ea9b268b7819359f3",
+    "url": "/static/js/runtime-main.786b8bbc.js"
+  }
+]);
